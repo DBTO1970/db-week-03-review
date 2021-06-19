45e1d51d7466a9e600a1a70a210281bc7566122e
dbto1970 <donbartojunior@gmail.com>
2021-06-19 10:54:42 -0400

    modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-// const myBtnEl = document.querySelector('.my-button');
-// const myTextEl = document.querySelector(".my-text");
+const myBtnEl = document.querySelector('.my-button');
+const myTextEl = document.querySelector(".my-text");
 
-// function swapSomeText() {
-//     myTextEl.textContent = myBtnEl.getAttribute('data-whatever');
-// }
+function swapSomeText() {
+    myTextEl.textContent = myBtnEl.getAttribute('data-whatever');
+}
 
-// myBtnEl.addEventListener('click', swapSomeText);
+myBtnEl.addEventListener('click', swapSomeText);
 
 $( document ).ready(function() {
     $(function () {
